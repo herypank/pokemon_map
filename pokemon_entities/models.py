5d42b06a848314abb5c4225b871b2adaaf17a57b
herypank <60302361+herypank@users.noreply.github.com>
2020-09-21 13:36:59 +0300

    Создал модель Pokemon.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Pokemon(models.Model):
+    title = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='pokemons', null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.title}"
+
+
 # your models here
