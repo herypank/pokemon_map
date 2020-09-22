e35794b618e74c5092066d2440f0c4a7c9833cd4
herypank <60302361+herypank@users.noreply.github.com>
2020-09-22 12:02:37 +0300

    Сделал чтобы photo обязательно заполнили

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='pokemons', null=True, blank=True)
+    photo = models.ImageField(upload_to='pokemons')
     
     def __str__(self):
         return f"{self.title}"
